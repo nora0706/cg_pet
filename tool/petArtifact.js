e63fb34dbd0fc1e782c05e0ca540d2fe3e2897ea
nora0706 <hungchunka.ck@gmail.com>
2022-08-19 22:10:11 +0900

    Add indentation to petlist.json

--- tool/petArtifact.js
+++ tool/petArtifact.js
@@ -6,5 +6,5 @@ const outputPath = path.join(__dirname, '..', 'public', 'pet_list.json');
 
 fs.readdir(petsPath, (err, files) => {
   const names = files.map(file => file.substring(0, file.length-('.json'.length)));
-  fs.writeFileSync(outputPath, JSON.stringify(names));
+  fs.writeFileSync(outputPath, JSON.stringify(names, null, 4));
 });
